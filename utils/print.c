056e23b20076c6343f9a0c48d445404af04d9d04
Antoine DESGRANGES <adesgran@student.42.fr>
2022-05-02 16:06:52 +0200

    Improve ft_print

--- utils/print.c
+++ utils/print.c
@@ -6,7 +6,7 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 14:19:25 by adesgran          #+#    #+#             */
-/*   Updated: 2022/05/02 14:55:48 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/05/02 16:06:37 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,15 @@
 
 void	ft_print(t_philo *philo, char *str)
 {
-	pthread_mutex_lock(&philo->table->print);
-	printf("%s", str);
-	pthread_mutex_unlock(&philo->table->print);
+	pthread_mutex_lock(&philo->table->lock_is_dead)
+	{
+		if (!philo->table->is_dead)
+		{
+			pthread_mutex_lock(&philo->table->print);
+			printf("%d %s", philo->number, str);
+			pthread_mutex_unlock(&philo->table->print);
+		}
+	}
+	pthread_mutex_unlock(&philo->table->lock_is_dead)
 	return ;
 }

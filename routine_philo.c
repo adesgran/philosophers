bc3a825cacb132f7c03dad2231985c32a837ec98
Antoine DESGRANGES <adesgran@student.42.fr>
2022-05-02 16:59:44 +0200

    Add check if dead

--- routine_philo.c
+++ routine_philo.c
@@ -6,7 +6,7 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 15:53:06 by adesgran          #+#    #+#             */
-/*   Updated: 2022/05/02 16:47:44 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/05/02 16:58:12 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static int	static_take_fork(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->fork);
-	if (ft_current_time() - philo->last_eat >= philo->table->time_die)
+	if (ft_current_time() - philo->last_eat >= philo->table->time_die || check_if_dead(philo))
 	{
 		pthread_mutex_unlock(&philo->fork);
 		kill_philo(philo);
@@ -24,7 +24,7 @@ static int	static_take_fork(t_philo *philo)
 	usleep(10);
 	ft_print(philo, "has taken a fork\n");
 	pthread_mutex_lock(&philo->previous->fork);
-	if (ft_current_time() - philo->last_eat >= philo->table->time_die)
+	if (ft_current_time() - philo->last_eat >= philo->table->time_die || check_if_dead(philo))
 	{
 		pthread_mutex_unlock(&philo->fork);
 		pthread_mutex_unlock(&philo->previous->fork);
@@ -69,8 +69,14 @@ void	*routine_philo(void* arg)
 		usleep(philo->table->time_eat * 1000);
 	while (1)
 	{
+		if (check_if_dead(philo))
+			return (NULL);
 		static_eat(philo);
+		if (check_if_dead(philo))
+			return (NULL);
 		static_sleep(philo);
+		if (check_if_dead(philo))
+			return (NULL);
 		static_think(philo);
 	}
 	return (NULL);

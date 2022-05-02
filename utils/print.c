181cd0492a8c34f022ea46ed90a91d9190b2c668
Antoine DESGRANGES <adesgran@student.42.fr>
2022-05-02 16:07:43 +0200

    Fix

--- utils/print.c
+++ utils/print.c
@@ -6,7 +6,7 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 14:19:25 by adesgran          #+#    #+#             */
-/*   Updated: 2022/05/02 16:06:37 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/05/02 16:07:39 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_print(t_philo *philo, char *str)
 {
-	pthread_mutex_lock(&philo->table->lock_is_dead)
+	pthread_mutex_lock(&philo->table->lock_is_dead);
 	{
 		if (!philo->table->is_dead)
 		{
@@ -23,6 +23,6 @@ void	ft_print(t_philo *philo, char *str)
 			pthread_mutex_unlock(&philo->table->print);
 		}
 	}
-	pthread_mutex_unlock(&philo->table->lock_is_dead)
+	pthread_mutex_unlock(&philo->table->lock_is_dead);
 	return ;
 }
